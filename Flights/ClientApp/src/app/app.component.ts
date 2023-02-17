afc6bbc71a83b502f1f3c7c49cff38444891d304
Fabian Kirchmann <Fabian_Kirchmann@users.noreply.github.com>
2023-02-17 15:54:54 +0100

    styling

--- Flights/ClientApp/src/app/app.component.ts
+++ Flights/ClientApp/src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.components.css']
 })
 export class AppComponent {
   title = 'app';
