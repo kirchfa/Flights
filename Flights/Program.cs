68593203e33acf1440c3c24bf8123e66ee2d8c8b
Fabian Kirchmann <Fabian_Kirchmann@users.noreply.github.com>
2023-02-26 16:27:30 +0100

    search filters and bugfixes

--- Flights/Program.cs
+++ Flights/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddDbContext<Entities>(options =>
 builder.Services.AddControllersWithViews();
 builder.Services.AddSwaggerGen(c =>
 {
+    c.DescribeAllParametersInCamelCase();
     c.AddServer(new OpenApiServer
     {
         Description = "Development Server",
